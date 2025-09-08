9a2831853f0ab86bc749cb04bf9bc15aec3aa7b7
StrikerEurika <ngentina69@gmail.com>
2025-09-08 09:59:32 +0700

    Arrange global layout and add footer

---
+++ src/app/components/ui/Layout.tsx
@@ -0,0 +1,13 @@
+import Header from "../Header";
+import React from "react";
+import Footer from "../Footer";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="bg-white flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+}
