0d6aef487f98934dee342f475d45092eac981126
StrikerEurika <ngentina69@gmail.com>
2025-09-08 13:36:43 +0700

    Add not found page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,32 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
-import Layout from "./components/ui/Layout";
-import HomePage from "./pages/HomePage";
-import About from "./components/ui/About";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import AppRouter from "./AppRouter";
 
-// export default function version
 export default function App() {
-  const [scrolled, setScrolled] = useState(false);
-
   useEffect(() => {
-    const handleScroll = () => setScrolled(window.scrollY > 50);
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    const handleScroll = () => {
+      // Perform any necessary actions on scroll here
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </Layout>
-        <Toaster richColors />
-      </Router>
+      <AppRouter />
+      <Toaster richColors />
     </ThemeProvider>
   );
 }
