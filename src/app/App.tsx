46668933ddf3a35ed895a2e7a5842ac65c8e6798
iamrith <chantharith77@gmail.com>
2025-11-11 18:32:10 +0700

    update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,12 @@
 import { Toaster } from '@/components/ui/sonner';
 import { ThemeProvider } from '@/components/theme-provider';
 import AppRouter from './AppRouter';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <SpeedInsights />
       <AppRouter />
       <Toaster richColors />
     </ThemeProvider>
