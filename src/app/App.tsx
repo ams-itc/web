65c706a383d1a85736716c282e33c5c2b38f82fc
iamrith <chantharith77@gmail.com>
2025-11-12 08:53:35 +0700

    update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,15 @@
 import { Toaster } from '@/components/ui/sonner';
 import { ThemeProvider } from '@/components/theme-provider';
 import AppRouter from './AppRouter';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { reportWebVitals } from './reportWebVitals';
 import { useEffect } from 'react';
 
 export default function App() {
   useEffect(() => {
-  reportWebVitals(console.log); // logs metrics in console
-}, []);
+    reportWebVitals(console.log);
+  }, []);
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <SpeedInsights />
